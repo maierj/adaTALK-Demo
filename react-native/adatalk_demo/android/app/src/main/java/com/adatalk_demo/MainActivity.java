c95deddf7328a5d80a8adaff8fb341683f2e728b
Jonas Maier <maierjonas@live.de>
2020-01-04 23:47:01 +0100

    Add empty react-native project

---
+++ react-native/adatalk_demo/android/app/src/main/java/com/adatalk_demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adatalk_demo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "adatalk_demo";
+  }
+}
